fc0006774bf3ca7e799f23db9cdc3ef3559ce802
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2025-05-04 23:23:11 -0300

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,41 @@
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const urlInput = document.getElementById("url").value;
+  const shortUrl = document.getElementById("shortUrl");
+  const button = document.getElementById("submit");
+
+  button.textContent = "Loading...";
+  button.disabled = true;
+
+  const response = await fetch("https://url-shortener-backend-sable.vercel.app/createShortUrl", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST",
+    },
+    body: JSON.stringify({ original_url: urlInput }),
+  });
+
+  button.textContent = "Create New Url";
+  button.disabled = false;
+
+  if (response.ok) {
+    const result = await response.json();
+
+    shortUrl.classList.add("text-green-500");
+    shortUrl.href = result.new_url;
+    shortUrl.innerHTML = result.new_url;
+    shortUrl.target = "_blank";
+    return;
+  }
+
+  shortUrl.innerHTML = "Failed to create new url";
+  shortUrl.target = "_self";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("form")?.addEventListener("submit", handleSubmit);
+});
